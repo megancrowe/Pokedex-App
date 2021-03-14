b76a07d83626425a9ebb73804d93b7062fe90f14
megancrowe <78937020+megancrowe@users.noreply.github.com>
2021-03-14 12:42:56 -0600

    Add Pokemon and properties to pokemonList
    script.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,15 +3,35 @@ let pokemonList = [
     name = 'Ninetales',
     height = 10,
     type = 'Fire',
+    HP = 73,
+    Attack = 76,
+    Defense = 75,
+    Speed = 100,
   }
   {
     name = 'Cubone',
     height = 4,
     type = 'Ground',
+    HP = 50,
+    Defense = 95,
+    Speed = 35,
   }
   {
     name = 'Wobbuffet',
     height = 13,
     type = 'Psychic',
+    HP = 190,
+    Attack = 33,
+    Defense = 58,
+    Speed = 33,
+  }
+  {
+    name = 'Taillow',
+    height = 3,
+    type = 'Flying', 'Normal',
+    HP = 40,
+    Attack = 55,
+    Defense = 30
+    Speed = 85,
   }
 ]
