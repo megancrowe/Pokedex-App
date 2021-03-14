3177a5ce197f2d2cd56b1b892da53987a9fb9919
megancrowe <78937020+megancrowe@users.noreply.github.com>
2021-03-14 12:33:07 -0600

    Add pokemonList array to script.js

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+let pokemonList = [
+  {
+    name = 'Ninetales',
+    height = 10,
+    type = 'Fire',
+  }
+  {
+    name = 'Cubone',
+    height = 4,
+    type = 'Ground',
+  }
+  {
+    name = 'Wobbuffet',
+    height = 13,
+    type = 'Psychic',
+  }
+]
