68fcb99fd008c5e31c0c63c1e7d08c8ec876d1c4
megancrowe <78937020+megancrowe@users.noreply.github.com>
2021-03-20 09:59:47 -0600

    Correct script.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,37 +2,38 @@ let pokemonList = [
   {
     name = 'Ninetales',
     height = 10,
-    type = 'Fire',
+    type = ['Fire'],
     HP = 73,
     Attack = 76,
     Defense = 75,
     Speed = 100,
-  }
+  },
   {
     name = 'Cubone',
     height = 4,
-    type = 'Ground',
+    type = ['Ground'],
     HP = 50,
     Attack = 50,
     Defense = 95,
     Speed = 35,
-  }
+  },
   {
     name = 'Wobbuffet',
     height = 13,
-    type = 'Psychic',
+    type = ['Psychic'],
     HP = 190,
     Attack = 33,
     Defense = 58,
     Speed = 33,
-  }
+  },
   {
     name = 'Taillow',
     height = 3,
-    type = 'Flying', 'Normal',
+    type = ['Flying', 'Normal'],
     HP = 40,
     Attack = 55,
-    Defense = 30
+    Defense = 30,
     Speed = 85,
   }
 ]
+
